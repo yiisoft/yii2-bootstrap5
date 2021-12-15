ee28bac009bd2cdf57ce8318410935c6673e2730
AIZAWA Hina <hina@fetus.jp>
2021-12-15 20:51:21 +0900

    Accept `Breadcrumbs::$homeLink = false` to omit "Home" link

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -40,7 +40,7 @@ class Breadcrumbs extends Widget
      */
     public $encodeLabels = true;
     /**
-     * @var array the first hyperlink in the breadcrumbs (called home link).
+     * @var array|false the first hyperlink in the breadcrumbs (called home link).
      * Please refer to [[links]] on the format of the link.
      * If this property is not set, it will default to a link pointing to [[\yii\web\Application::homeUrl]]
      * with the label 'Home'. If this property is false, the home link will not be rendered.
@@ -113,7 +113,7 @@ class Breadcrumbs extends Widget
                 'label' => 'Home',
                 'url' => '/',
             ], $this->itemTemplate);
-        } else {
+        } elseif ($this->homeLink !== false) {
             $links[] = $this->renderItem($this->homeLink, $this->itemTemplate);
         }
 
