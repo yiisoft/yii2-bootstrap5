df2d2d7ffbaae9e8adba73c0b32719c3f59bde88
Kirill Petrov <archibeardrinker@gmail.com>
2021-12-16 20:26:58 +0400

    Update BootstrapIconAsset.php

--- src/BootstrapIconAsset.php
+++ src/BootstrapIconAsset.php
@@ -2,7 +2,7 @@
 
 namespace app\assets;
 
-class IconsBs5Asset extends \yii\web\AssetBundle
+class BootstrapIconAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@vendor/twbs/bootstrap-icons/font';
 
