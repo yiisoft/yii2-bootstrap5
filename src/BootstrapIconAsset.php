26c001763d91679069142dfe3b942eaba61db64b
Kirill Petrov <archibeardrinker@gmail.com>
2021-12-16 20:08:08 +0400

    Update and rename BootstrapIconAsset to BootstrapIconAsset.php

---
+++ src/BootstrapIconAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\assets;
+
+class IconsBs5Asset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/twbs/bootstrap-icons/font';
+
+    public $css = [
+        'bootstrap-icons.css'
+    ];
+
+}
