dede0982fc92c41c2c93bbe5f730c5fb1bfb657a
Anton <info@ensostudio.ru>
2021-12-22 22:37:52 +0300

    Update Progress.php

--- src/Progress.php
+++ src/Progress.php
@@ -166,15 +166,15 @@ class Progress extends Widget
     }
 
     /**
-     * Generates a bar
+     * Generates a progress bar.
      * @param int $percent the percentage of the bar
-     * @param string $label , optional, the label to display at the bar
+     * @param string $label optional, the label to display at the bar
      * @param array $options the HTML attributes of the bar
      * @return string the rendering result.
      */
     protected function renderBar(int $percent, string $label = '', array $options = []): string
     {
-        $percent = (float)trim(rtrim((string)$percent, '%'));
+
         $options = array_merge($options, [
             'role' => 'progressbar',
             'aria' => [
