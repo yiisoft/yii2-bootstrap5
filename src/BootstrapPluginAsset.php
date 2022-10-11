a042751045459f43445c5f6b5a838d738edbf817
Anton <info@ensostudio.ru>
2022-10-11 08:07:24 +0300

    Update BootstrapPluginAsset
    - Load minified JS on production
    - Optimize list of publishes files
    - Normalize comments

--- src/BootstrapPluginAsset.php
+++ src/BootstrapPluginAsset.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license https://www.yiiframework.com/license/
  */
 
 declare(strict_types=1);
@@ -12,17 +12,30 @@ namespace yii\bootstrap5;
 use yii\web\AssetBundle;
 
 /**
- * Asset bundle for the Twitter bootstrap javascript files.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
+ * Twitter Bootstrap 5 JavaScript bundle.
  */
 class BootstrapPluginAsset extends AssetBundle
 {
+    /**
+     * @inheritDoc
+     */
     public $sourcePath = '@bower/bootstrap/dist';
+    /**
+     * @inheritDoc
+     */
     public $js = [
-        'js/bootstrap.bundle.js',
+        YII_ENV_PROD ? 'js/bootstrap.bundle.min.js' : 'js/bootstrap.bundle.js'
     ];
+    /**
+     * @inheritDoc
+     */
+    public $publishOptions => [
+        'only' => ['js/bootstrap.bundle.*']
+    ];
+    /**
+     * @inheritDoc
+     */
     public $depends = [
-        BootstrapAsset::class,
+        BootstrapAsset::class
     ];
 }
