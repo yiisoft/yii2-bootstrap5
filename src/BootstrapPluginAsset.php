db6208a2d7a55826c4e3e098271434a922ff733c
Anton <info@ensostudio.ru>
2022-10-13 15:13:20 +0300

    Update BootstrapPluginAsset (#51)
    * Update BootstrapPluginAsset
    
    - Load minified JS on production
    - Optimize list of publishes files
    - Normalize comments
    
    * Update BootstrapPluginAsset.php
    
    * Update BootstrapPluginAsset.php
    
    * Update CHANGELOG.md

--- src/BootstrapPluginAsset.php
+++ src/BootstrapPluginAsset.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license https://www.yiiframework.com/license/
  */
 
 declare(strict_types=1);
@@ -12,17 +12,30 @@ namespace yii\bootstrap5;
 use yii\web\AssetBundle;
 
 /**
- * Asset bundle for the Twitter bootstrap javascript files.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
+ * Twitter Bootstrap 5 JavaScript bundle.
  */
 class BootstrapPluginAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/bootstrap/dist';
+    /**
+     * @inheritDoc
+     */
+    public $sourcePath = '@bower/bootstrap';
+    /**
+     * @inheritDoc
+     */
     public $js = [
-        'js/bootstrap.bundle.js',
+        YII_ENV_PROD ? 'dist/js/bootstrap.bundle.min.js' : 'dist/js/bootstrap.bundle.js'
     ];
+    /**
+     * @inheritDoc
+     */
+    public $publishOptions = [
+        'only' => ['dist/js/bootstrap.bundle.*', 'js/src/*.js', 'js/src/*/*.js']
+    ];
+    /**
+     * @inheritDoc
+     */
     public $depends = [
-        BootstrapAsset::class,
+        BootstrapAsset::class
     ];
 }
