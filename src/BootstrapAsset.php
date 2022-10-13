3111ba58fa9be22a5f5360413770abfb378c4af2
Anton <info@ensostudio.ru>
2022-10-13 15:11:49 +0300

    Update BootstrapAsset (#52)
    * Update BootstrapAsset
    
    - Load minified JS on production
    - Optimize list of publishes files
    - Normalize comments
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md

--- src/BootstrapAsset.php
+++ src/BootstrapAsset.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license https://www.yiiframework.com/license/
  */
 
 declare(strict_types=1);
@@ -11,11 +11,26 @@ namespace yii\bootstrap5;
 
 use yii\web\AssetBundle;
 
+/**
+ * Twitter Bootstrap 5 CSS bundle.
+ */
 class BootstrapAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/bootstrap/dist';
+    /**
+     * @inheritDoc
+     */
+    public $sourcePath = '@bower/bootstrap';
+    /**
+     * @inheritDoc
+     */
     public $css = [
-        'css/bootstrap.css',
+        YII_ENV_PROD ? 'dist/css/bootstrap.min.css' : 'dist/css/bootstrap.css'
     ];
-    public $js = [];
+    /**
+     * @inheritDoc
+     */
+    public $publishOptions = [
+        'only' => ['scss/*.scss', 'scss/*/*.scss', 'dist/css/bootstrap.*'],
+        'except' => ['scss/bootstrap-*.scss']
+    ];
 }
