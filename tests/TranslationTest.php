39729bf3a70e6484a40d9e09b0b9f92cc5fb7e73
Maksim Spirkov <63721828+mspirkov@users.noreply.github.com>
2026-09-24 17:15:17 +0300

    Check the code style fully on CI (#119)

--- tests/TranslationTest.php
+++ tests/TranslationTest.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+/**
  * @package yii2-bootstrap5
  * @author Simon Karlen <simi.albi@gmail.com>
  */
